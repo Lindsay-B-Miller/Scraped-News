8252e8ad0177e033ec34ba9d41ec9acee694c488
Lindsay <lindsay.lbm@gmail.com>
2019-11-08 14:02:44 -0600

    notes saving in database

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -23,10 +23,12 @@ router.put("/articles/", function (req, res) {
         { _id: id },
         { $set: { saved: true } },
         function (data) {
+
             res.json(data);
         }
     );
 });
+
 
 router.get("/saved", function (req, res) {
     db.Article.find({
@@ -40,8 +42,23 @@ router.get("/saved", function (req, res) {
                 article: found
             }
         }
-        res.render("index", articles)
+        res.render("saved", articles)
     });
 })
+
+router.post("/articles/", function (req, res) {
+    var id = req.body.id
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({ _id: id }, {
+                $push: { note: dbNote._id }
+            }, { new: true });
+        }).then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
 
 module.exports = router;
--- public/app.js
+++ public/app.js
@@ -11,3 +11,21 @@ $(document).on("click", ".saveBtn", function () {
         console.log(data);
     })
 })
+
+$(document).on("click", ".saveNote", function () {
+    var thisId = $(this).attr("data-id");
+    console.log(thisId)
+
+    $.ajax({
+        method: "POST",
+        url: "/articles/",
+        data: {
+            id: thisId,
+            title: $(".titleInput").val(),
+            body: $(".bodyInput").val()
+        }
+    }).then(function (data) {
+        console.log(data);
+    })
+})
+
